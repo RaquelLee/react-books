a5c5e0dad488386e6eb3276c8f6e94d23ec64971
rsco <RaquelLee@users.noreply.github.com>
2021-05-20 18:37:45 -0400

    get route added for google books

--- react-books/routes/api/books.js
+++ react-books/routes/api/books.js
@@ -1,10 +1,13 @@
 const router = require("express").Router();
 const booksController = require("../../controllers/booksController");
 
+router.route("/googleBooks")
+  .get(booksController.getGoogleBooks)
+
 // Matches with "/api/books"
 router.route("/")
   .get(booksController.findAll)
-  .post(booksController.create);
+// .post(booksController.create);
 
 // Matches with "/api/books/:id"
 router
