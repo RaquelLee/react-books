41622e4e728c463569ed9231203a9e5edc329fe1
rsco <RaquelLee@users.noreply.github.com>
2021-05-20 18:38:07 -0400

    model updated for google book saves

--- react-books/models/book.js
+++ react-books/models/book.js
@@ -2,11 +2,11 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const bookSchema = new Schema({
-  authors: { type: String, required: true },
+  authors: Array,
   description: String,
   image: String,
   link: String,
-  title: { type: String, required: true }
+  title: String,
 });
 
 const Book = mongoose.model("Book", bookSchema);
