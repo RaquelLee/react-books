7aa2c7562093e84b9cb5c81b8e0393e77e1d4472
rsco <RaquelLee@users.noreply.github.com>
2021-05-20 18:40:41 -0400

    displays saved books

---
+++ react-books/client/src/pages/Saved.js
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from "react";
+import API from "../utils/API";
+import { Link } from "react-router-dom";
+import { Col, Row, Container } from "../components/Grid";
+import { List, ListItem } from "../components/List";
+// gets books from db
+function Books() {
+  // Setting our component's initial state
+  const [books, setBooks] = useState([])
+
+  // Load all books and store them with setBooks
+  useEffect(() => {
+    loadBooks()
+  }, [])
+
+  // Loads all books and sets them to books
+  function loadBooks() {
+    API.getBooks()
+      .then(res => setBooks(res.data)
+      ).catch(err => console.log(err));
+  };
+
+  return (
+    <Container fluid>
+      <Row>
+        <Col size="md-6 sm-12">
+          <h1>Your Saved Books</h1>
+          {books.length ? (
+            <List>
+              {books.map(book => (
+                <ListItem key={book.id}>
+                  <Link to={book.link}>
+                    <strong>
+                      View {book.title} by 
+                      {book.authors}
+                    </strong>
+                  </Link>
+                  <img src={book.image} 
+                  alt={book.title}></img>
+                  <button
+                    Delete
+                  />
+                </ListItem>
+              ))}
+            </List>
+          ) : (
+            <h3>No Results to Display</h3>
+          )}
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+//backend console logs show up in terminal
+export default Books;
