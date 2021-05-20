a5e91c792281aef75fd136a0ca3574e3f042e1ba
rsco <RaquelLee@users.noreply.github.com>
2021-05-20 18:39:24 -0400

    get google books method added with fetch and api key

--- react-books/controllers/booksController.js
+++ react-books/controllers/booksController.js
@@ -1,21 +1,35 @@
 const db = require("../models");
+const fetch = require("node-fetch");
+const apikey = process.env.API_KEY;
 
 // Defining methods for the booksController
 module.exports = {
-  findAll: function(req, res) {
+  getGoogleBooks: function (req, res) {
+    fetch("https://www.googleapis.com/books/v1/volumes?q=title:"
+      + req.title + "&printType=books&key=" + apikey +
+      "&fields=items(selfLink,volumeInfo(title,authors,description,imageLinks(thumbnail)))")
+      .then(response => {
+        return response.json()
+      }).then(data => {
+        res.json(data)
+      }).catch(err => {
+        res.json(err)
+      });
+  },
+  findAll: function (req, res) {
     db.Book
       .find(req.query)
       .sort({ date: -1 })
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
-  findById: function(req, res) {
+  findById: function (req, res) {
     db.Book
       .findById(req.params.id)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
-  remove: function(req, res) {
+  remove: function (req, res) {
     db.Book
       .findById({ _id: req.params.id })
       .then(dbModel => dbModel.remove())
