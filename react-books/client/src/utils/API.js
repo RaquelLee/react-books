84570d0f0ee1a542bfe63e14d38aa29fe4c9df9c
rsco <RaquelLee@users.noreply.github.com>
2021-05-20 20:45:42 -0400

    title sent to axios get

--- react-books/client/src/utils/API.js
+++ react-books/client/src/utils/API.js
@@ -3,7 +3,8 @@ import axios from "axios";
 
 export default {
   getGoogleBooks: function (title) {
-    return axios.get("/api/books/googleBooks", title)
+    console.log(title)
+    return axios.get("/api/books/googleBooks/" + title)
   },
   // getOneGoogleBook: function(id) {
   //   return axios.get(
